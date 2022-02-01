31721cc6cced872c4cdfdd8ab403f28f8bee9c17
fikrigatrh <fikrigatra4@gmail.com>
2022-02-01 23:55:34 +0700

    first commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module bitbucket.org/service-ekspedisi
+
+go 1.17
+
+require github.com/fikrigatrh/create-skeleton v1.0.2
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	gtr_skeleton "github.com/fikrigatrh/create-skeleton"
+)
+
+func main() {
+	gtr_skeleton.CreateTon("create_ton")
+}
