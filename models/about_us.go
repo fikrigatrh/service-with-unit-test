edb44e5cf8a4dc19d0309ec517026242f4e18479
fikrigatrh <fikrigatra4@gmail.com>
2022-02-27 15:08:36 +0700

    update

--- models/about_us.go
+++ models/about_us.go
@@ -15,3 +15,20 @@ type Footer struct {
 
 type Header struct {
 }
+
+type Booking struct {
+	NamaPengirim             string `json:"nama_pengirim"`
+	Email                    string `json:"email" validate:"email"`
+	NoTelpPengirim           string `json:"no_telp_pengirim" validate:"numeric"`
+	KotaPenjemputan          string `json:"kota_penjemputan"`
+	AlamatLengkapPenjemputan string `json:"alamat_lengkap_penjemputan"`
+	NamaPenerima             string `json:"nama_penerima"`
+	NoTelpPenerima           string `json:"no_telp_penerima" validate:"numeric"`
+	KotaPenerima             string `json:"kota_penerima"`
+	AlamatPenerima           string `json:"alamat_penerima"`
+	JenisBarang              string `json:"jenis_barang"`
+	PerkiraanBerat           string `json:"perkiraan_berat" validate:"numeric"`
+	JumlahKoli               string `json:"jumlah_koli" validate:"numeric"`
+	Kubikasi                 string `json:"kubikasi"`
+	Keterangan               string `json:"keterangan"`
+}
