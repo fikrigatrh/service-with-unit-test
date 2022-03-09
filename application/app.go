a93e25adf23a23d1f3910fa7f3c4f29045d364ee
fikrigatrh <fikrigatra4@gmail.com>
2022-03-10 02:38:31 +0700

    update pagination

--- application/app.go
+++ application/app.go
@@ -27,10 +27,10 @@ import (
 func StartApp() {
 	router := gin.New()
 	router.Use(gin.Recovery())
-	err := router.SetTrustedProxies([]string{"0.0.0.0"})
-	if err != nil {
-		return
-	}
+	//errs := router.SetTrustedProxies([]string{"0.0.0.0"})
+	//if errs != nil {
+	//	return
+	//}
 
 	env.NewEnv(".env")
 
@@ -45,7 +45,7 @@ func StartApp() {
 	models.InitTable(dbBase.DB)
 
 	// init db log
-	err = dbBase.DB.AutoMigrate(models.Logs{})
+	err := dbBase.DB.AutoMigrate(models.Logs{})
 	if err != nil {
 		return
 	}
