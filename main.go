ae7e11307c20d7cc851bba7ab61fda69f45ade18
fikrigatrh <fikrigatra4@gmail.com>
2022-03-10 02:38:59 +0700

    update pagination

--- main.go
+++ main.go
@@ -6,30 +6,4 @@ import (
 
 func main() {
 	application.StartApp()
-	//switch etaTemp[1] {
-	//case "01":
-	//	etaTemp[1] = models.January
-	//case "02":
-	//	etaTemp[1] = models.February
-	//case "03":
-	//	etaTemp[1] = models.March
-	//case "04":
-	//	etaTemp[1] = models.April
-	//case "05":
-	//	etaTemp[1] = models.May
-	//case "06":
-	//	etaTemp[1] = models.June
-	//case "07":
-	//	etaTemp[1] = models.July
-	//case "08":
-	//	etaTemp[1] = models.August
-	//case "09":
-	//	etaTemp[1] = models.September
-	//case "10":
-	//	etaTemp[1] = models.October
-	//case "11":
-	//	etaTemp[1] = models.November
-	//case "12":
-	//	etaTemp[1] = models.December
-	//}
 }
